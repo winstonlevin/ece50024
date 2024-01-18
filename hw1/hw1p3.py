4290cff8641547ef66b534c095b41e30def42cc5
Winston C. Levin <winstonclevin@gmail.com>
2024-01-18 12:07:41 -0500

    Bugged linprog implementation for hw1p3f

--- hw1/hw1p3.py
+++ hw1/hw1p3.py
@@ -12,6 +12,7 @@ l3 = sp.special.eval_legendre(3, x)
 l4 = sp.special.eval_legendre(4, x)
 l_poly = np.vstack((np.ones(l1.shape), l1, l2, l3, l4))
 beta = np.array((-0.001, 0.01, 0.55, 1.5, 1.2)).reshape((-1, 1))
+n_beta = beta.shape[0]
 
 np.random.seed(3)
 n_samples = 50
@@ -61,5 +62,30 @@ ax.set_xlabel(r'$x$')
 ax.set_ylabel(r'$y$')
 fig_lstsq_outlier.tight_layout()
 fig_lstsq_outlier.savefig('p3d.svg', format='svg')
+
+# Exercise 3 (f) -------------------------------------------------------------------------------------------------------
+c_linprog = np.vstack((np.zeros((n_beta, 1)), np.ones((n_samples, 1))))
+eye_n_samples = np.eye(n_samples)
+a_linprog = np.hstack((np.vstack((design_matrix, -design_matrix)), np.vstack((eye_n_samples, -eye_n_samples))))
+b_linprog = np.vstack((output_outlier, -output_outlier))
+
+a_eq_linprog = np.hstack((design_matrix, eye_n_samples))
+b_eq_linprog = output_outlier
+
+# sol = sp.optimize.linprog(c_linprog, a_linprog, b_linprog, bounds=(None, None))
+sol = sp.optimize.linprog(c_linprog, A_eq=a_eq_linprog, b_eq=b_eq_linprog, bounds=(-50, 50))
+beta_hat_linprog = sol.x[0:n_beta]
+y_hat_linprog = (beta_hat_linprog.T @ l_poly).flatten()
+
+# Plot
+fig_linprog = plt.figure()
+ax = fig_linprog.add_subplot(111)
+ax.scatter(x, output_outlier.flatten())
+ax.plot(x, y_hat_linprog, color=cols[1])
+ax.grid()
+ax.set_xlabel(r'$x$')
+ax.set_ylabel(r'$y$')
+fig_linprog.tight_layout()
+fig_linprog.savefig('p3d.svg', format='svg')
 
 plt.show()
