b6c124889f29b083acf30788a2d32f009dccb099
winstonlevin <wlevin@purdue.edu>
2024-03-29 09:48:35 -0400

    Evaluate new canonical model, save data, and compile running list of results and accuracies for ensemble prediciton.

--- minichallenge/eval_model.py
+++ minichallenge/eval_model.py
@@ -16,8 +16,9 @@ model = model.to(device)
 # Choose best params
 
 state_dict_arr = np.asarray(model.state_dicts, dtype=object)
-training_acc_arr = np.asarray(model.test_accuracies)
-model.load_state_dict(state_dict_arr.flatten()[training_acc_arr.argmax()])
+validation_acc_arr = np.asarray(model.test_accuracies)
+model.load_state_dict(state_dict_arr.flatten()[validation_acc_arr.argmax()])
+validation_acc = validation_acc_arr.flatten()[validation_acc_arr.argmax()]
 transform = transforms.Resize((model.n_pixels, model.n_pixels))
 
 image_read_mode = ImageReadMode.GRAY
@@ -41,6 +42,7 @@ test_loader = torch.utils.data.DataLoader(dataset_test, batch_size=batch_size, s
 # Evaluate model ----------------------------------------------------------------------------------------------------- #
 idx = 0
 n_batches = len(test_loader)
+print(f'--- Evaluating Model with {validation_acc/100:%} Accuracy on Validation Set ---')
 for current_batch, (inputs, _) in enumerate(test_loader, start=1):
     if current_batch % 10 == 1:
         print(f'Testing batch {current_batch}/{n_batches}...')
@@ -54,8 +56,35 @@ for current_batch, (inputs, _) in enumerate(test_loader, start=1):
 # Save data in correct format ---------------------------------------------------------------------------------------- #
 image_id_arr = np.arange(0, len(images_test), 1)
 celebrity_names_arr = categories_arr[targets_test]
+file_name = 'test_results.csv'
+
+# Compare to previous results
+n_test = len(images_test)
+try:
+    previous_celebrity_names = np.loadtxt(file_name, dtype=str, skiprows=1, usecols=1, delimiter=',')
+    num_changed = n_test - np.sum(celebrity_names_arr == previous_celebrity_names)
+    print(f'Saving new results to "{file_name}", '
+          f'{num_changed}/{n_test} ({num_changed/n_test:.2%}) changed from previous results.')
+except FileNotFoundError as _:
+    print(f'Saving results to "{file_name}".')
+
+# Append to running list of all results
+file_name_compilation = 'compiled_test_results.csv'
+celeb_names_with_header = np.vstack((
+    np.asarray((validation_acc,), dtype=str).reshape((-1, 1)),
+    celebrity_names_arr.reshape((-1, 1))
+))
+try:
+    previous_results_all = np.loadtxt(file_name_compilation, dtype=str, delimiter=',').reshape((n_test+1, -1))
+    results_all = np.hstack((previous_results_all, celeb_names_with_header))
+    print('Appending new results to compiled results...')
+except FileNotFoundError as _:
+    print('Generating a compiled results...')
+    results_all = celeb_names_with_header
+np.savetxt(file_name_compilation, results_all, fmt='%s', delimiter=',')
+
 data_to_save = np.hstack((
     np.asarray(image_id_arr, dtype=str).reshape((-1, 1)),
     celebrity_names_arr.reshape((-1, 1))
 ))
-np.savetxt('test_results.csv', data_to_save, fmt='%s', delimiter=',', header='Id,Category', comments='')
+np.savetxt(file_name, data_to_save, fmt='%s', delimiter=',', header='Id,Category', comments='')
