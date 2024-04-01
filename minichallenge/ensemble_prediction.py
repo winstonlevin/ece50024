2ef72cd6551d51de740797ac6235c93994c07e9c
winstonlevin <wlevin@purdue.edu>
2024-04-01 10:42:18 -0400

    Increase accuracy to about 60 percent with ensemble prediction. Need to train more models to hopefully get better accuracy.

---
+++ minichallenge/ensemble_prediction.py
@@ -0,0 +1,95 @@
+import numpy as np
+
+categories_arr = np.loadtxt('category.csv', delimiter=',', skiprows=1, usecols=1, dtype=str)
+n_categories, = categories_arr.shape
+
+eval_data = np.loadtxt('compiled_test_results.csv', dtype=str, delimiter=',')
+eval_accuracies = np.asarray(eval_data[0, :], dtype=float)
+eval_predictions = eval_data[1:, :]
+n_output, n_models = eval_predictions.shape
+
+
+def ensemble_predictor(predictions, accuracies):
+    n_output, n_models = predictions.shape
+
+    # Determine how alike models are
+    similarity_mat = np.empty(shape=(n_models, n_models))
+    for idx in range(n_models):
+        similarity_mat[idx, :] = np.sum(predictions[:, idx:idx + 1] == predictions, axis=0) / n_output
+
+    # Use uniqueness of prediction to weight less similar models more heavily
+    uniqueness_mat = np.linalg.inv(similarity_mat)
+
+    # Convert predictions to integer and determine model similarities
+    prediction_arr = np.zeros((n_output, n_models, n_categories), dtype=int)
+    for idx in range(n_categories):
+        prediction_arr[:, :, idx] = (predictions == categories_arr[idx]) * accuracies
+
+    predictions_ensemble = (uniqueness_mat @ prediction_arr).sum(axis=1).argmax(axis=1)
+
+    return predictions_ensemble
+
+
+# Run predictor on validation set and determine accuracy
+validation_data = np.loadtxt('compiled_validation_results.csv', dtype=str, delimiter=',')
+validation_accuracies = np.asarray(validation_data[0, :], dtype=float)
+validation_predictions = validation_data[1:, :]
+
+predictions_validation = ensemble_predictor(validation_predictions, validation_accuracies)
+targets_validation = np.loadtxt('data_validation.csv', delimiter=',', usecols=0, dtype=int)
+
+accuracy_ensemble = (predictions_validation == targets_validation).sum() / len(targets_validation)
+
+# Run predictor on testing set
+eval_data = np.loadtxt('compiled_test_results.csv', dtype=str, delimiter=',')
+eval_accuracies = np.asarray(eval_data[0, :], dtype=float)
+eval_predictions = eval_data[1:, :]
+
+predictions_test = ensemble_predictor(eval_predictions, eval_accuracies)
+
+# Save data in correct format ---------------------------------------------------------------------------------------- #
+celebrity_names_arr = categories_arr[predictions_test]
+file_name = 'test_results.csv'
+
+try:
+    previous_celebrity_names = np.loadtxt(file_name, dtype=str, skiprows=1, usecols=1, delimiter=',')
+    num_changed = n_output - np.sum(celebrity_names_arr == previous_celebrity_names)
+    print(f'Saving new results to "{file_name}", '
+          f'{num_changed}/{n_output} ({num_changed/n_output:.2%}) changed from previous results.')
+except FileNotFoundError as _:
+    print(f'Saving results to "{file_name}".')
+
+image_id_arr = np.arange(0, len(eval_predictions[:, 0]), 1)
+data_to_save = np.hstack((
+    np.asarray(image_id_arr, dtype=str).reshape((-1, 1)),
+    celebrity_names_arr.reshape((-1, 1))
+))
+np.savetxt(file_name, data_to_save, fmt='%s', delimiter=',', header='Id,Category', comments='')
+
+# Determine how alike models are
+similarity_mat = np.empty(shape=(n_models, n_models))
+for idx in range(n_models):
+    similarity_mat[idx, :] = np.sum(eval_predictions[:, idx:idx+1] == eval_predictions, axis=0) / n_output
+
+# Use uniqueness of prediction to weight less similar models more heavily
+uniqueness_mat = np.linalg.inv(similarity_mat)
+
+# Convert predictions to integer and determine model similarities
+prediction_arr = np.zeros((n_output, n_models, n_categories), dtype=int)
+for idx in range(n_categories):
+    prediction_arr[:, :, idx] = (eval_predictions == categories_arr[idx]) * eval_accuracies
+
+# Create weighted prediction from the multiple models
+predictions_ensemble = (uniqueness_mat @ prediction_arr).sum(axis=1).argmax(axis=1)
+
+# Save data in correct format ---------------------------------------------------------------------------------------- #
+celebrity_names_arr = categories_arr[predictions_ensemble]
+file_name = 'test_results.csv'
+
+try:
+    previous_celebrity_names = np.loadtxt(file_name, dtype=str, skiprows=1, usecols=1, delimiter=',')
+    num_changed = n_output - np.sum(celebrity_names_arr == previous_celebrity_names)
+    print(f'Saving new results to "{file_name}", '
+          f'{num_changed}/{n_output} ({num_changed/n_output:.2%}) changed from previous results.')
+except FileNotFoundError as _:
+    print(f'Saving results to "{file_name}".')
--- minichallenge/eval_model.py
+++ minichallenge/eval_model.py
@@ -9,17 +9,24 @@ from model_classes import ImageDataset
 
 with open('canonical/model_8CNN.pickle', 'rb') as file:
     model = pickle.load(file)
-# with open('canonical/transform_acc50.pickle', 'rb') as file:
-#     transform = pickle.load(file)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = model.to(device)
-# Choose best params
 
-state_dict_arr = np.asarray(model.state_dicts[-1], dtype=object)
+# Choose best params
 validation_acc_arr = np.asarray(model.test_accuracies[-1])
-model.load_state_dict(state_dict_arr.flatten()[validation_acc_arr.argmax()])
-validation_acc = validation_acc_arr.flatten()[validation_acc_arr.argmax()]
-transform = transforms.Resize((model.n_pixels, model.n_pixels))
+if hasattr(model, 'state_dicts'):
+    state_dict_arr = np.asarray(model.state_dicts[-1], dtype=object)
+
+    model.load_state_dict(state_dict_arr.flatten()[validation_acc_arr.argmax()])
+    validation_acc = validation_acc_arr.flatten()[validation_acc_arr.argmax()]
+else:
+    validation_acc = validation_acc_arr.flatten()[-1]
+
+if hasattr(model, 'n_pixels'):
+    transform = transforms.Resize((model.n_pixels, model.n_pixels))
+else:
+    with open('canonical/transform_acc50.pickle', 'rb') as file:
+        transform = pickle.load(file)
 
 image_read_mode = ImageReadMode.GRAY
 
@@ -39,44 +46,40 @@ dataset_test = ImageDataset(
 batch_size = 64
 test_loader = torch.utils.data.DataLoader(dataset_test, batch_size=batch_size, shuffle=False)
 
-# Evaluate model ----------------------------------------------------------------------------------------------------- #
+# Run model on validation set with chosen parameters ----------------------------------------------------------------- #
+root_validation = '../tmp/minichallenge_data/train_cropped/'
+images_validation = np.loadtxt('data_validation.csv', delimiter=',', usecols=1, dtype=str)
+targets_validation = np.loadtxt('data_validation.csv', delimiter=',', usecols=0, dtype=int)
+dataset_validation = ImageDataset(
+    root=root_validation, images=images_validation, targets=targets_validation, n_targets=n_categories,
+    transform=transform, image_read_mode=image_read_mode
+)
+validation_loader = torch.utils.data.DataLoader(dataset_validation, batch_size=batch_size, shuffle=False)
+n_validation, = targets_validation.shape
+prediction_validation = np.empty(shape=targets_validation.shape, dtype=int)
+
 idx = 0
-n_batches = len(test_loader)
-print(f'--- Evaluating Model with {validation_acc/100:%} Accuracy on Validation Set ---')
-for current_batch, (inputs, _) in enumerate(test_loader, start=1):
+n_batches = len(validation_loader)
+print(f'--- Validating Model with {validation_acc/100:%} Accuracy on Validation Set ---')
+for current_batch, (inputs, _) in enumerate(validation_loader, start=1):
     if current_batch % 10 == 1:
-        print(f'Testing batch {current_batch}/{n_batches}...')
+        print(f'Validation batch {current_batch}/{n_batches}...')
     inputs = inputs.to(device)
     outputs = model(inputs)
     _, prediction = torch.max(outputs.data, 1)
     n_predicted = len(prediction)
-    targets_test[idx:idx + n_predicted] = prediction.cpu()
+    prediction_validation[idx:idx + n_predicted] = prediction.cpu()
     idx += n_predicted
 
-# Save data in correct format ---------------------------------------------------------------------------------------- #
-image_id_arr = np.arange(0, len(images_test), 1)
-celebrity_names_arr = categories_arr[targets_test]
-file_name = 'test_results.csv'
-
-# Compare to previous results
-n_test = len(images_test)
-try:
-    previous_celebrity_names = np.loadtxt(file_name, dtype=str, skiprows=1, usecols=1, delimiter=',')
-    num_changed = n_test - np.sum(celebrity_names_arr == previous_celebrity_names)
-    print(f'Saving new results to "{file_name}", '
-          f'{num_changed}/{n_test} ({num_changed/n_test:.2%}) changed from previous results.')
-except FileNotFoundError as _:
-    print(f'Saving results to "{file_name}".')
-
-# Append to running list of all results
-# (Assume the test accuracy will by 98% of the validation accuracy)
-file_name_compilation = 'compiled_test_results.csv'
+# Save compiled validation results
+celebrity_names_arr = categories_arr[prediction_validation]
+file_name_compilation = 'compiled_validation_results.csv'
 celeb_names_with_header = np.vstack((
-    np.asarray((0.98*validation_acc,), dtype=str).reshape((-1, 1)),
+    np.asarray((validation_acc,), dtype=str).reshape((-1, 1)),
     celebrity_names_arr.reshape((-1, 1))
 ))
 try:
-    previous_results_all = np.loadtxt(file_name_compilation, dtype=str, delimiter=',').reshape((n_test+1, -1))
+    previous_results_all = np.loadtxt(file_name_compilation, dtype=str, delimiter=',').reshape((n_validation+1, -1))
     results_all = np.hstack((previous_results_all, celeb_names_with_header))
     print('Appending new results to compiled results...')
 except FileNotFoundError as _:
@@ -84,8 +87,53 @@ except FileNotFoundError as _:
     results_all = celeb_names_with_header
 np.savetxt(file_name_compilation, results_all, fmt='%s', delimiter=',')
 
-data_to_save = np.hstack((
-    np.asarray(image_id_arr, dtype=str).reshape((-1, 1)),
-    celebrity_names_arr.reshape((-1, 1))
-))
-np.savetxt(file_name, data_to_save, fmt='%s', delimiter=',', header='Id,Category', comments='')
+# # Evaluate model ----------------------------------------------------------------------------------------------------- #
+# idx = 0
+# n_batches = len(test_loader)
+# print(f'--- Evaluating Model with {validation_acc/100:%} Accuracy on Validation Set ---')
+# for current_batch, (inputs, _) in enumerate(test_loader, start=1):
+#     if current_batch % 10 == 1:
+#         print(f'Testing batch {current_batch}/{n_batches}...')
+#     inputs = inputs.to(device)
+#     outputs = model(inputs)
+#     _, prediction = torch.max(outputs.data, 1)
+#     n_predicted = len(prediction)
+#     targets_test[idx:idx + n_predicted] = prediction.cpu()
+#     idx += n_predicted
+#
+# # Save data in correct format ---------------------------------------------------------------------------------------- #
+# image_id_arr = np.arange(0, len(images_test), 1)
+# celebrity_names_arr = categories_arr[targets_test]
+# file_name = 'test_results.csv'
+#
+# # Compare to previous results
+# n_test = len(images_test)
+# try:
+#     previous_celebrity_names = np.loadtxt(file_name, dtype=str, skiprows=1, usecols=1, delimiter=',')
+#     num_changed = n_test - np.sum(celebrity_names_arr == previous_celebrity_names)
+#     print(f'Saving new results to "{file_name}", '
+#           f'{num_changed}/{n_test} ({num_changed/n_test:.2%}) changed from previous results.')
+# except FileNotFoundError as _:
+#     print(f'Saving results to "{file_name}".')
+#
+# # Append to running list of all results
+# # (Assume the test accuracy will by 98% of the validation accuracy)
+# file_name_compilation = 'compiled_test_results.csv'
+# celeb_names_with_header = np.vstack((
+#     np.asarray((0.98*validation_acc,), dtype=str).reshape((-1, 1)),
+#     celebrity_names_arr.reshape((-1, 1))
+# ))
+# try:
+#     previous_results_all = np.loadtxt(file_name_compilation, dtype=str, delimiter=',').reshape((n_test+1, -1))
+#     results_all = np.hstack((previous_results_all, celeb_names_with_header))
+#     print('Appending new results to compiled results...')
+# except FileNotFoundError as _:
+#     print('Generating a compiled results...')
+#     results_all = celeb_names_with_header
+# np.savetxt(file_name_compilation, results_all, fmt='%s', delimiter=',')
+#
+# data_to_save = np.hstack((
+#     np.asarray(image_id_arr, dtype=str).reshape((-1, 1)),
+#     celebrity_names_arr.reshape((-1, 1))
+# ))
+# np.savetxt(file_name, data_to_save, fmt='%s', delimiter=',', header='Id,Category', comments='')
